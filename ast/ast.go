5b6481674216435a34046c37c7e3c3d022ec089d
Maksym <bienkowski.maksym@gmail.com>
2024-09-02 22:23:28 +0200

    initial asp & parser impl, parsing let statements

---
+++ ast/ast.go
@@ -0,0 +1,59 @@
+package ast
+
+import "interpreter/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (*LetStatement) statementNode()          {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (*Identifier) expressionNode()        {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type BasicExpression struct {
+	Tokens []token.Token
+}
+
+func (*BasicExpression) expressionNode() {}
+func (be *BasicExpression) TokenLiteral() string {
+	var rv string
+	for _, t := range be.Tokens {
+		rv += t.Literal
+	}
+	return rv
+}
